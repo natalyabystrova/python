926b96cb27797df4b66ca7dd71be013483ab868d
geekbrains <info@geekbrains.ru>
2022-02-05 23:48:27 +0300

    init

--- lesson-3/task_2.py
+++ lesson-3/task_2.py
@@ -1,19 +1,18 @@
-# # Задание 2.
-# def num_translate_adv(k):
-#     kl = k.lower()
-#     d1 = {'one': "один", 'two': "два", 'three': "три",
-#           'four': "четыре", 'five': "пять", 'six': "шесть",
-#           'seven': "семь", 'eight': "восемь", 'nine': "девять", 'ten': "десять"}
-#     if k in d1.keys():
-#         return d1[k]
-#     elif kl in d1.keys() and k.istitle():
-#         return d1[kl].capitalize()
-#
-#
-# result_1 = num_translate_adv('Eight')
-# result_2 = num_translate_adv('one')
-# result_3 = num_translate_adv('random')
-#
-# print(result_1, type(result_1))
-# print(result_2, type(result_2))
-# print(result_3, type(result_3))
+def num_translate_adv(k):
+    kl = k.lower()
+    d1 = {'one': "один", 'two': "два", 'three': "три",
+          'four': "четыре", 'five': "пять", 'six': "шесть",
+          'seven': "семь", 'eight': "восемь", 'nine': "девять", 'ten': "десять"}
+    if k in d1.keys():
+        return d1[k]
+    elif kl in d1.keys() and k.istitle():
+        return d1[kl].capitalize()
+
+
+result_1 = num_translate_adv('Eight')
+result_2 = num_translate_adv('one')
+result_3 = num_translate_adv('random')
+
+print(result_1, type(result_1))
+print(result_2, type(result_2))
+print(result_3, type(result_3))
--- lesson-3/task_3.py
+++ lesson-3/task_3.py
@@ -1,14 +1,15 @@
-# # Задание 3.
-#
-# def tresaurus(*words):
-#     result = {}
-#     for word in words:
-#         first_letter = word[0]
-#         if first_letter not in result.keys():
-#             result[first_letter] = [word]
-#         else:
-#             result[first_letter].append(word)
-#     return result
-#
-# answer = tresaurus("Иван", "Екатерина", "Ольга", "Илья")
-# print(answer, type(answer))
+
+def tresaurus(*words):
+    result = {}
+    for word in words:
+        first_letter = word[0]
+        if first_letter not in result.keys():
+            result[first_letter] = [word]
+        else:
+            result[first_letter].append(word)
+    return result
+
+
+answer = tresaurus("Иван", "Екатерина", "Ольга", "Илья")
+
+print(answer, type(answer))

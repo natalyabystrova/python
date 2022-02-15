222f4f4b2dce7ba8484c79ea7282c91b62202a04
geekbrains <info@geekbrains.ru>
2022-02-15 21:11:38 +0300

    исправлено

--- lesson-5/task_5.py
+++ lesson-5/task_5.py
@@ -1,16 +1,19 @@
 src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 
+"""убирает все повтряющиеся элементы:"""
+_set = set()
+tmp = set()
+for x in src:
+    if x not in tmp:
+        _set.add(x)
+    else:
+        _set.discard(x)
+    tmp.add(x)
+unique_set = [x for x in src if x in _set]
+print(unique_set)
 
-# решение со списками:
-new_lst = []
-[new_lst.append(i) for i in src if i not in new_lst]
-print(new_lst)
 
-
-# с использованием словаря:
-print(list(dict.fromkeys(src)))
-
-# с множествами:
+""" с множествами (c сохранением одного элемента из повторяющихся):"""
 def my_set(*args):
     _set = set()
     l = []
@@ -22,3 +25,14 @@ def my_set(*args):
 
 
 print(my_set(*src))
+
+
+
+# решение со списками:
+new_lst = []
+[new_lst.append(i) for i in src if i not in new_lst]
+print(new_lst)
+
+
+# с использованием словаря:
+print(list(dict.fromkeys(src)))

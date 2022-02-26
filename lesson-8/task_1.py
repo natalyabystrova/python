1c4aae5a7141aff09264e5a72c44b27d1b94a515
geekbrains <info@geekbrains.ru>
2022-02-26 23:51:31 +0300

    changed 2

--- lesson-8/task_1.py
+++ lesson-8/task_1.py
@@ -1,13 +1,17 @@
 import re
+
 # email_address = """geek-brains@mail.ru"""
 email = re.compile(r"""(?P<name>\-{0,}?\+{0,}?\'{0,}?\.{0,}?\w+?\+{0,}?\-
 {0,}?\.{0,}?\'{0,}?\w+?)@
-(?P<domain>\w+\.{0,}?\-{0,}?\w+)""", re.VERBOSE)                             #domain
+(?P<domain>\w+\.{0,}?\-{0,}?\w+)""", re.VERBOSE)
+
+
 def email_parse(email_address):
-        rez = email.search(email_address)
-        if not rez:
-            raise ValueError("wrong email: 'email_address'")
-        print(rez.groupdict())
-        return rez.groupdict()
-email_parse("geek.bra.ins@mail.ru")
-    # print(rez.groupdict())rez.groupdict
+    res = email.search(email_address)
+    if not res:
+        raise ValueError("wrong email: 'email_address'")
+    print(res.groupdict())
+    return res.groupdict()
+
+
+email_parse("geekbra.ins@mail.ru")

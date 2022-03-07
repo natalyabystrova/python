3911eb983e14fba7e6f18eb0bae05e0a5a98a628
geekbrains <info@geekbrains.ru>
2022-03-07 14:09:46 +0300

    changed 3v agfbzb

--- lesson-10/task_2.py
+++ lesson-10/task_2.py
@@ -1,1 +1,29 @@
+from abc import ABC, abstractmethod
+class Clothes(ABC):
+    def __init__(self, value):
+        self.value = value
+
+    @abstractmethod
+    def consumption(self):
+        pass
+
+    def __str__(self):
+        return str(self.value)
+
+class Coat(Clothes):
+    @property
+    def consumption(self):
+        return self.value / 6.5 + 0.5
+
+class Suit(Clothes):
+    @property
+    def consumption(self):
+        return self.value * 2 + 0.3
+
+c1 = Coat(30)
+c2 = Suit(1.7)
+print(c1)
+print(c1.consumption)
+print(c2.consumption)
+
 

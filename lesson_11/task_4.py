402abd09b93026ca98483b7b07a89c4a1296d0aa
geekbrains <info@geekbrains.ru>
2022-03-14 15:46:48 +0300

    round result

--- lesson_11/task_4.py
+++ lesson_11/task_4.py
@@ -4,7 +4,12 @@ class Storage:
         self.dicts = {}
 
     def reception(self, equipment):
-        if equipment in
+        if self.dicts.get(equipment):
+            self.dicts[equipment] += 1
+        else:
+            self.dicts[equipment] = 1
+        print(f' Товар {equipment} был принят на склад.'
+              f' Текущее количество товаров на складе: {self.dicts}')
 
 class Office_equipment:
     def __init__(self, name, model, color):
@@ -12,11 +17,18 @@ class Office_equipment:
         self.model = model
         self.color = color
 
+    def __str__(self,):
+        return f'Название оборудования: {self.name} \n Модель:' \
+               f'{self.model} \n Цвет : {self.color}'
+
 class Printer(Office_equipment):
     def __init__(self, name, model, color, print_speed):
         super().__init__(name, model, color)
         self.print_speed = print_speed
 
+    def __str__(self,):
+        return f'{Office_equipment.__str__(self)}\n Скорость принтера:' \
+               f'{self.print_speed}'
 class Scanner(Office_equipment):
     def __init__(self, name, model, color, permission):
         super().__init__(name, model, color)
@@ -26,3 +38,5 @@ class Copier(Office_equipment):
     def __init__(self, name, model, color, scaling):
         super().__init__(name, model, color)
         self.scaling = scaling
+
+c1 = Printer(Canon, x100, red, 400)

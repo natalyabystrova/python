a4062b117306ec9944256881b16c3d546e747dcb
geekbrains <info@geekbrains.ru>
2022-03-14 16:21:21 +0300

    round result

--- lesson_11/task_4.py
+++ lesson_11/task_4.py
@@ -2,13 +2,12 @@ class Storage:
     def __init__(self, name):
         self.name = name
         self.dicts = {}
-
-    def reception(self, equipment):
-        if self.dicts.get(equipment):
-            self.dicts[equipment] += 1
+    def reception(self):
+        if self.dicts.get(self.name):
+            self.dicts[self.name] += 1
         else:
-            self.dicts[equipment] = 1
-        print(f' Товар {equipment} был принят на склад.'
+            self.dicts[self.name] = 1
+        print(f' Товар {self.name} был принят на склад.'
               f' Текущее количество товаров на складе: {self.dicts}')
 
 class Office_equipment:
@@ -34,9 +33,28 @@ class Scanner(Office_equipment):
         super().__init__(name, model, color)
         self.permission = permission
 
+    def __str__(self,):
+        return f'{Office_equipment.__str__(self)}\n Разрешение сканера:' \
+               f'{self.permission}'
+
 class Copier(Office_equipment):
     def __init__(self, name, model, color, scaling):
         super().__init__(name, model, color)
         self.scaling = scaling
 
-c1 = Printer(Canon, x100, red, 400)
+    def __str__(self,):
+        return f'{Office_equipment.__str__(self)}\n Масштабирование ксерокса:' \
+               f'{self.scaling}'
+
+c1 = Printer('Canon', 'x100', 'red', '400')
+print(c1)
+c2 = Scanner('Nikon', 'x200', 'black', '1000')
+print(c2)
+c3 = Copier('Xiomi', 'x500', 'white', 'yes')
+print(c3)
+
+c4 = Storage('Samsung')
+c4.reception()
+c5 = Storage('Siemens')
+c5.reception()
+

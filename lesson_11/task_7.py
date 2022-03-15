f8ae76709442acf5eac077f8aff823a6051961ba
geekbrains <info@geekbrains.ru>
2022-03-15 16:36:09 +0300

    round result

--- lesson_11/task_7.py
+++ lesson_11/task_7.py
@@ -1,11 +1,36 @@
-class Complex_number:
-    def __init__(self, a, b, *args):
-        self.a = a
-        self.b = b
-        self.z = 'a + b * i'
+class Complex:
+    def __init__(self, real, imag=0):
+        self.complex = complex(real, imag)
 
     def __str__(self):
-        return f'z = self.a + self.b * i'
+        return self.complex.__str__()
 
     def __add__(self, other):
-        return
+        if isinstance(other, Complex):
+            other = other.complex
+
+        complex = self.complex + other
+        return Complex(complex.real, int(complex.imag))
+
+    def __mul__(self, other):
+        if isinstance(other, Complex):
+            other = other.complex
+
+        complex = self.complex * other
+        return Complex(complex.real, int(complex.imag))
+
+c1 = Complex(5, 4)
+c2 = Complex(-2, 3)
+print(c1, c2)
+print(c1 + c2)
+print(c1 * c2)
+
+
+
+
+
+
+
+
+
+

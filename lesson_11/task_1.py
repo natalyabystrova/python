1dbae083917b8000cd29d574b45bd37c3b315152
geekbrains <info@geekbrains.ru>
2022-03-22 16:22:03 +0300

    round result

--- lesson_11/task_1.py
+++ lesson_11/task_1.py
@@ -1,6 +1,6 @@
 class Date:
     def __init__(self, date):
-        self.date = str(date)
+            self.date = str(date)
 
     @classmethod
     def extract(cls, date):
@@ -20,14 +20,16 @@ class Date:
 
 
     def __str__(self):
-        return f'Введенная дата : {self.date}'
+        a, b, c = (Date.extract(self.date))
+        return f'Введенная дата : {str(c)}.{str(b).zfill(2)}.{str(a).zfill(2)}'
 
 
 c1 = Date('14-03-2022')
 print(c1)
-print(Date.extract('12-03-2022'))
+# print(Date.extract('12-03-2022'))
 print(Date.valid('12-12-2000'))
 print(Date.valid('32-12-2000'))
-print(Date.valid('12-00-2000'))
-print(Date.valid('12-12-2025'))
+# print(Date.valid('12-00-2000'))
+# print(Date.valid('12-12-2025'))
+
 

e5fd28234817a416fed4b4a7773765e5e97f6d77
geekbrains <info@geekbrains.ru>
2022-03-23 15:38:31 +0300

    round result

--- lesson_11/task_1.py
+++ lesson_11/task_1.py
@@ -1,6 +1,17 @@
+import time
 class Date:
     def __init__(self, date):
-            self.date = str(date)
+        self.date = str(date)
+
+    def __str__(self):
+        a, b, c = (Date.extract(self.date))
+        if Date.valid(self.date):
+            return f' {str(c)}.{str(b).zfill(2)}.{str(a).zfill(2)}' \
+                   f' \nДата {self.date}, результат: дата введена правильно!'
+
+        else:
+            return f' {str(c)}.{str(b).zfill(2)}.{str(a).zfill(2)}' \
+                   f' \nДата {self.date}, результат: дата введена неправильно!'
 
     @classmethod
     def extract(cls, date):
@@ -9,27 +20,58 @@ class Date:
 
     @staticmethod
     def valid(date):
-        if Date.extract(date)[0] not in range(1, 32):
-            return f'Дата введена неправильно: день должен быть в интервале 1-31.'
-        elif Date.extract(date)[1] not in range(1, 13):
-            return f'Дата введена неправильно: месяц должен быть в интервале 1-12.'
-        elif Date.extract(date)[2] not in range(1900, 2023):
-            return f'Дата введена неправильно: год должен быть в интервале 1900-2022.'
+        try:
+            valid_date = time.strptime(date, '%d-%m-%Y')
+        except ValueError:
+            return False
         else:
-            return f'Дата введена правильно.'
+            return True
 
-
-    def __str__(self):
-        a, b, c = (Date.extract(self.date))
-        return f'Введенная дата : {str(c)}.{str(b).zfill(2)}.{str(a).zfill(2)}'
-
-
-c1 = Date('14-03-2022')
+c1 = Date('11-03-1899')
 print(c1)
-# print(Date.extract('12-03-2022'))
-print(Date.valid('12-12-2000'))
-print(Date.valid('32-12-2000'))
-# print(Date.valid('12-00-2000'))
-# print(Date.valid('12-12-2025'))
+c2 = Date('12-13-1899')
+print(c2)
+
+
+# import time
+# class Date:
+#     def __init__(self, date):
+#         self.date = date
+#         for d in self.date:
+#             d = str(d)
+#
+#     # def __str__(self):
+#     #     a, b, c = (Date.extract(self.date))
+#     #     if Date.valid(self.date):
+#     #         return f' {str(c)}.{str(b).zfill(2)}.{str(a).zfill(2)}' \
+#     #                f' \nДата {self.date}, результат: дата введена правильно!'
+#     #
+#     #     else:
+#     #         return f' {str(c)}.{str(b).zfill(2)}.{str(a).zfill(2)}' \
+#     #                f' \nДата {self.date}, результат: дата введена неправильно!'
+#     #
+#     @classmethod
+#     def extract(cls, date):
+#         for d in date:
+#             res = d.split('-')
+#             return int(res[0]), int(res[1]), int(res[2])
+#     #
+#     # @staticmethod
+#     # def valid(date):
+#     #     try:
+#     #         valid_date = time.strptime(date, '%d-%m-%Y')
+#     #     except ValueError:
+#     #         return False
+#     #     else:
+#     #         return True
+#
+# c1 = Date(['11-03-1891', '11-13.1892'])
+# print(c1)
+# print(Date.extract(c1))
+#
+
+
+
+
 
 

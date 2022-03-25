2cbc2628712d6893e1d86c69f1f61ae5f2e88437
geekbrains <info@geekbrains.ru>
2022-03-25 23:33:51 +0300

    refactor 3

--- lesson_11/task_3.py
+++ lesson_11/task_3.py
@@ -1,16 +1,23 @@
 class OwnError(Exception):
     pass
+
+
+def append(n):
+    if n.isdigit():
+        lst.append(int(number))
+        print(lst)
+        return True
+    elif number == "stop":
+        print(f'Программа завершена. {lst}')
+        return False
+    else:
+        raise OwnError("Вы ввели не число")
+
 lst = []
-while True:
+is_run = True
+while is_run:
     try:
         number = input("Введите число")
-        if number.isdigit():
-            lst.append(int(number))
-            print(lst)
-        elif number == "stop":
-            print(f'Программа завершена. {lst}')
-            break
-        else:
-            raise OwnError("Вы ввели не число")
+        is_run = append(number)
     except OwnError:
         print("Вы ввели не число. Введите число.")
